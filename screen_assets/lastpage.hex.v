01f8e18c8b706fb85805a78a1844faebe2a2e655
Syed Muhammad Ali Naqvi <sn07590@st.habib.edu.pk>
2023-02-07 22:21:11 +0500

    assets

---
+++ screen_assets/lastpage.hex.v
@@ -0,0 +1,21 @@
+
+    module rom2(clk, video_on, x, y, color);
+    parameter ROM_WIDTH = 12;
+    parameter ROM_ADDR_BITS = 19;
+    (* rom_style="block" *)
+    reg [ROM_WIDTH-1:0] rom [(2**ROM_ADDR_BITS)-1:0];
+    input wire clk;
+    input wire video_on;
+    input wire [9:0] x;
+    input wire [8:0] y;
+    reg [ROM_ADDR_BITS-1:0] address;
+    output reg [ROM_WIDTH-1:0] color;
+    initial
+      $readmemh("lastpage.hex", rom);
+    always @(posedge clk)
+      if (video_on) begin
+         address <= {y, x};
+         color <= rom[address];
+      end
+    endmodule
+    
